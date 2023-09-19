325e410090bccac397df5cb878d05f3cf25557f9
SyedMohdHaris <smh261290@gmail.com>
2023-09-19 02:48:07 -0700

    Migrations-Haris

--- database/migrations/2023_09_19_082024_create_teams_table.php
+++ database/migrations/2023_09_19_082024_create_teams_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->nullable(false);
+            $table->unsignedBigInteger('adminId');
+            $table->string('description')->nullable(false);
             $table->timestamps();
+        
+            $table->foreign('adminId')->references('id')->on('users')->onDelete('cascade');
+            
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teams');
     }
 };
